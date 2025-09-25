78b58a52b34de54c73875b0118f639620f961fdb
Kristel Anne S. Pabon <kristel0726@gmail.com>
2025-09-25 19:18:46 +0800

    lab act 3 changes

--- app/controllers/classlists_controller.rb
+++ app/controllers/classlists_controller.rb
@@ -25,6 +25,12 @@ class ClasslistsController < ApplicationController
 
     respond_to do |format|
       if @classlist.save
+        @classlist.section.increment!(:number_of_students)
+        @classlist.student.increment!(:number_of_units, @classlist.section.subject.number_of_units)
+
+        totalAssessment = (@classlist.student.number_of_units * 2500) + 15000
+        @classlist.student.update(total_assessment: totalAssessment)
+
         format.html { redirect_to @classlist, notice: "Classlist was successfully created." }
         format.json { render :show, status: :created, location: @classlist }
       else
@@ -50,6 +56,11 @@ class ClasslistsController < ApplicationController
   # DELETE /classlists/1 or /classlists/1.json
   def destroy
     @classlist.destroy!
+    @classlist.section.decrement!(:number_of_students)
+    @classlist.student.decrement!(:number_of_units, @classlist.section.subject.number_of_units)
+
+    totalAssessment = (@classlist.student.number_of_units * 2500) + 15000
+    @classlist.student.update(total_assessment: (@classlist.student.number_of_units * 2500) + 15000)
 
     respond_to do |format|
       format.html { redirect_to classlists_path, notice: "Classlist was successfully destroyed.", status: :see_other }
